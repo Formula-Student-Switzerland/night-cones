34c1ccb23d5a37e21740142d1915233293d84633
daniw <dani.winz@bluewin.ch>
2024-01-16 00:28:44 +0100

    FailSafe Schematic mostly completed

--- HW/scripts/night-cone.m
+++ HW/scripts/night-cone.m
@@ -9,10 +9,16 @@ close all;
 nof_LED = 21;
 % LED protocol
 LED_buffer = 24;
+% WS2813B
 t_0_H = 400e-9;
 t_0_L = 850e-9;
 t_1_H = 800e-9;
 t_1_L = 450e-9;
+% WS2812C
+t_0_H = 300e-9;
+t_0_L = 1090e-9;
+t_1_H = 1090e-9;
+t_1_L = 320e-9;
 t_reset = 50e-6;
 failsafe_timeout = 1e-3;
 failsafe_fps = 1000;
@@ -44,7 +50,7 @@ Counter_Protocol = 2 .* Protocol_length;
 Counter_t_reset = 2 .* ceil(t_reset ./ (t_1_H + t_1_L));
 Counter_fps = ceil(1./ failsafe_fps ./ (t_1_H + t_1_L));
 Counter_min = max([Counter_Protocol, Counter_t_reset, Counter_fps]);
-Counter_output = ceil(log(Counter_min)/log(2));
+Counter_output = ceil(log(Counter_min)/log(2) - 1);
 
 disp('== Reset Generator Counter ==');
 disp(['Use counter output Q' num2str(Counter_output)]);
