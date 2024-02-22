ac01779b2180eaa1c4eb3416555a89c676868eee
Sowintuu <o_clemens@posteo.de>
2024-02-22 15:38:33 +0100

    Implemented led representation for testing.

--- SW/client/testing.c
+++ SW/client/testing.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 void decodeColors (int color, int brightness, int *decoded);
 void displayLeds (int *leds, int *colors);
@@ -91,9 +92,49 @@ void decodeColors (int color, int brightness, int *decoded) {
 
 }
 
-void displayLeds(int *leds, int *colors) {
-	
+/*
+U8/L2	U9/L3	U10/L4	U11/L5	U12/L6
 
+U7/L1	U22/L16			U23/L17	U13/L7
 
+U6/L0							U14/L8
+
+U21/L15	U25/19			U24/L18	U15/L9
+
+U20/L14	U19/L13	U18/L12	U17/L11	U16/L10
+
+X X X X X
+X X   X X
+X       X
+X X   X X
+X X X X X
+*/
+int displayLeds(int *leds, int *colors, int lastLen) {
+	char displayStr[100];
+	char illumChar[21];
+	int stringLen;
+
+	// Set array for LED on strings.
+	for (int k=0; k<20; k++) {
+		if (leds[k] == 1) {
+			illumChar[k] = "X";
+		} else {
+			illumChar[k] = " ";
+		}
+	}
+	illumChar[20] = "\0"
+
+	// Add color header.
+	displayStr = sprintf("R: %03d\nG: %03d\nB: %03d\n\n", colors[0], colors[1], colors[2]);
+
+	// Add led representation.
+	strcat(displayStr, sprintf("%c %c %c %c %c\n\n", illumChar[2], illumChar[3], illumChar[4], illumChar[5], illumChar[6]))
+	strcat(displayStr, sprintf("%c %c   %c %c\n\n", illumChar[1], illumChar[16], illumChar[17], illumChar[7]))
+	strcat(displayStr, sprintf("%c       %c\n\n", illumChar[0], illumChar[8]))
+	strcat(displayStr, sprintf("%c %c   %c %c\n\n", illumChar[15], illumChar[19], illumChar[18], illumChar[9]))
+	strcat(displayStr, sprintf("%c %c %c %c %c", illumChar[14], illumChar[13], illumChar[12], illumChar[11], illumChar[10]))
+
+	// Calculate display string length.
+	stringLen = sizeof(displayStr) / sizeof(displayStr[0])
 
 }
