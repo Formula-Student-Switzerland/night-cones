3377d1970ebeef6205483b2b7a0b4f6002bb0cf4
Sowintuu <o_clemens@posteo.de>
2024-02-23 16:04:18 +0100

    Implemented functions representing light modes. - Adjusted dev console representation accordingly - Added constant for DEV mode

--- SW/client/testing.c
+++ SW/client/testing.c
@@ -2,36 +2,83 @@
 #include <string.h>
 #include <unistd.h>
 
+#define DEV 1
+#define LED_NUMBER 20
+
 void decodeColors (int color, int brightness, int *decoded);
-int displayLeds(int *leds, int *colors);
+void setup();
+void loop();
+void processLightModes (int lightMode, int color, int brightness, int repetitionTime, int *ledState);
 
+#ifdef DEV
+	int displayLeds(int *leds, int *colors);
+#endif
 
-int main() {
-	int lastLen = 0;
-	int leds[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-
-	// Define debug settings.
-	int color = 130;
-	int brightness = 50;
-
-	// Decode colors and brightness.
-	int decoded[3];
-	decodeColors(color, brightness, decoded);
-
-	// Show led representation.
-	while (1) {
+#ifdef DEV
+	int main() {
 		
-		displayLeds(leds, decoded);
-		sleep(1);
-		leds[0] = 0;
-		displayLeds(leds, decoded);
-		sleep(1);
-		leds[0] = 1;
+		setup();
+		loop();
+
+		return 0;
+	}
+#endif
+
+
+void setup() {
 
 	}
 
-	return 0;
+
+void loop() {
+	// Receive and process serial data.
+	int lightMode = 0;
+	int color = 90;
+	int brightness = 100;
+	int repetitionTime = 500;  // ms
+
+	// Process led representation.
+	int ledState[LED_NUMBER*3] = {0};
+
+	processLightModes(lightMode, color, brightness, repetitionTime, ledState);
+
+	// Control LED.
+	#ifndef DEV
+
+	#else
+		displayLeds(ledState);
+	#endif
+
 }
+
+void processLightModes (int lightMode, int color, int brightness, int repetitionTime, int *ledState) {
+
+	switch (lightMode) {
+		case 0: 
+			lightMode1(color, brightness, ledState);
+			break;
+		case 1:
+			lightMode2(color, brightness, ledState)
+
+
+	}
+
+}
+
+void lightMode1(int color, int brightness, int *ledState) {
+	// Process colors.
+	int colorDecoded[3];
+	decodeColors(color, brightness, colorDecoded);
+
+	// Assign colors to LED.
+	for (int k=0; k<LED_NUMBER; k++) {
+		ledState[k] = colorDecoded[0];
+		ledState[k+1] = colorDecoded[1];
+		ledState[k+2] = colorDecoded[2];
+	}
+
+}
+
 
 
 void decodeColors (int color, int brightness, int *decoded) {
@@ -39,73 +86,60 @@ void decodeColors (int color, int brightness, int *decoded) {
 	float red = 0;
 	float green = 0;
 	float blue = 0;
-	int red_scaled = 0;
-	int green_scaled = 0;
-	int blue_scaled = 0;
 
 	// Set red component.
 	if (color <= 95) {
     	red = 0;
 	} else if (color <= 127) {
-    	red = (color - 95) / 32.0 * 15;
+    	red = (color - 95) / 32.0 * 30;
   	} else {
-    	red = 15;
+    	red = 30;
   	}
 
 	// Set green component.
   	if (color >= 64 && color <= 127) {
-    	green = 15;
+    	green = 30;
   	} else if (color >= 32 && color <= 63) {
-    	green = (color - 31) / 32.0 * 15;
+    	green = (color - 31) / 32.0 * 30;
   	} else if (color >= 128 && color <= 191) {
-    	green = (1 - (color - 127) / 64.0) * 15;
+    	green = (1 - (color - 127) / 64.0) * 30;
   	} else if (color >= 224) {
-    	green = (color - 223) / 32.0 * 15;
+    	green = (color - 223) / 32.0 * 30;
   	} else {
     	green = 0;
   	}
 
   	// Set blue component.
   	if ((color >= 32 && color <= 63) || (color >= 224)) {
-    	blue = 15;
+    	blue = 30;
   	} else if (color <= 31) {
-    	blue = color / 31.0 * 15;
+    	blue = color / 31.0 * 30;
   	} else if (color >= 64 && color <= 95) {
-    	blue = (1 - (color - 63) / 32.0) * 15;
+    	blue = (1 - (color - 63) / 32.0) * 30;
   	} else if (color >= 192 && color <= 223) {
-    	blue = (color - 191) / 32.0 * 15;
+    	blue = (color - 191) / 32.0 * 30;
   	} else {
     	blue = 0;
   	}
 
-/*
-	printf("%f\n", red);
-	printf("%f\n", green);
-	printf("%f\n", blue);
-*/
-
-
   	// Scale colors with brightness.
   	if (red >= green && red >= blue) {
-    	red_scaled = 2.55 * brightness;
-    	green_scaled = green / red * 2.55 * brightness;
-    	blue_scaled = blue / red * 2.55 * brightness;
+    	decoded[0] = 1.275 * brightness;  // red scaled
+    	decoded[1] = green / red * 1.275 * brightness;  // green scaled
+    	decoded[2] = blue / red * 1.275 * brightness;  // blue scaled
   	} else if (green >= blue) {
-    	green_scaled = 2.55 * brightness;
-    	red_scaled = red / green * 2.55 * brightness;
-    	blue_scaled = blue / green * 2.55 * brightness;
+    	decoded[1] = 1.275 * brightness;
+    	decoded[0] = red / green * 1.275 * brightness;
+    	decoded[2] = blue / green * 1.275 * brightness;
   	} else {
-    	blue_scaled = 2.55 * brightness;
-    	red_scaled = red / blue * 2.55 * brightness;
-    	green_scaled = green / blue * 2.55 * brightness;
+    	decoded[2] = 1.275 * brightness;
+    	decoded[0] = red / blue * 1.275 * brightness;
+    	decoded[1] = green / blue * 1.275 * brightness;
   	}
-
-	decoded[0] = red_scaled;
-	decoded[1] = green_scaled;
-	decoded[2] = blue_scaled;
 
 }
 
+#ifdef DEV
 /*
 U8/L2	U9/L3	U10/L4	U11/L5	U12/L6
 
@@ -123,18 +157,19 @@ X       X
 X X   X X
 X X X X X
 */
-int displayLeds(int *leds, int *colors) {
+int displayLeds(int *ledState) {
 	char displayStr[100];
-	char illumChar[21];
-	int stringLen;
+	char illumChar[LED_NUMBER*3+1];
 
 	// Delete the last conlose content.
 	printf("\033[H\033[J");
 
 	// Set array for LED on strings.
-	for (int k=0; k<20; k++) {
-		if (leds[k] == 1) {
+	for (int k=0; k<LED_NUMBER; k++) {
+		if (ledState[k] > 50 || ledState[k+1] > 50 || ledState[k+2] > 50) {
 			illumChar[k] = 'X';
+		} else if (ledState[k] > 0 || ledState[k+1] > 0 || ledState[k+2] > 0) {
+			illumChar[k] = 'x';
 		} else {
 			illumChar[k] = ' ';
 		}
@@ -142,7 +177,7 @@ int displayLeds(int *leds, int *colors) {
 	illumChar[20] = '\0';
 
 	// Add color header.
-	sprintf(displayStr, "R: %03d\nG: %03d\nB: %03d\n\n", colors[0], colors[1], colors[2]);
+	sprintf(displayStr, "R: %03d\nG: %03d\nB: %03d\n\n", ledState[0], ledState[1], ledState[2]);
 
 	// Add led representation.
 	sprintf(displayStr, "%s%c %c %c %c %c\n", displayStr, illumChar[2], illumChar[3], illumChar[4], illumChar[5], illumChar[6]);
@@ -151,11 +186,9 @@ int displayLeds(int *leds, int *colors) {
 	sprintf(displayStr, "%s%c %c   %c %c\n", displayStr, illumChar[15], illumChar[19], illumChar[18], illumChar[9]);
 	sprintf(displayStr, "%s%c %c %c %c %c", displayStr, illumChar[14], illumChar[13], illumChar[12], illumChar[11], illumChar[10]);
 
-	// Calculate display string length.
-	stringLen = sizeof(displayStr) / sizeof(displayStr[0]);
-
 	printf(displayStr);
 
 	return 0;
 
 }
+#endif
