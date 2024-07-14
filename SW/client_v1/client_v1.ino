6d909ce0deecea55f9b934b99bc753d1ac1ab006
daniw <dani.winz@bluewin.ch>
2024-07-14 02:24:06 +0200

    SW: Wifi connection only at startup if magnet present, optical indication of temperature and battery voltage when magnet present

--- SW/client_v1/client_v1.ino
+++ SW/client_v1/client_v1.ino
@@ -37,7 +37,8 @@
 #define FREQ_LOOP 3000
 
 #define WIFI_ATTEMPTS 1
-
+#define WIFI_DELAY_MS 20
+#define WIFI_LOOPS    250
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -45,10 +46,11 @@ const int PIN = 2;
 
 const int DELAY = 3000;
 
-const char *ssid = "FSCH_CONES";
-const char *password = "I_R4C3@night";
+const char *ssid = "Malve_B4";
+const char *password = "malve3988";
 const char *ota_pwd = "NC_update";
 
+bool wifi_configure = true;
 bool wifi_connected = false;
 
 void setup() {
@@ -67,90 +69,103 @@ void setup() {
     strip.setPixelColor(n, 50, 0, 0);
   };
   strip.show();
-
-  // Wifi Setup
-  Serial.begin(115200);
-  Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  int wifi_attempt = 0;
-
-  esp_led_blink(FREQ_ON, 4);
-
-  delay(1000);
-
-  while (WiFi.waitForConnectResult() != WL_CONNECTED && wifi_attempt < WIFI_ATTEMPTS) {
-    Serial.println("Connection Failed! Rebooting...");
-    //delay(5000);
-    //ESP.restart();
-    wifi_attempt++;
+  
+  bool wifi_loop_continue = false;
+  bool hall_status;
+  unsigned int wifi_loop_cnt = 0;
+  while (wifi_loop_cnt < WIFI_LOOPS & wifi_configure) {
+    strip.show();
+    delay(WIFI_DELAY_MS);
+    hall_status = digitalRead(HALL_PIN);
+    if(hall_status) {
+      wifi_configure = false;
+    }
+    wifi_loop_cnt++;
   }
+  if (wifi_configure) {
+    // Wifi Setup
+    Serial.begin(115200);
+    Serial.println("Booting");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    int wifi_attempt = 0;
+
+    esp_led_blink(FREQ_ON, 4);
+
+    delay(1000);
+
+    while (WiFi.waitForConnectResult() != WL_CONNECTED && wifi_attempt < WIFI_ATTEMPTS) {
+      Serial.println("Connection Failed! Rebooting...");
+      delay(5000);
+      ESP.restart();
+      wifi_attempt++;
+    }
 
 
-  // Check Wifi and init OTA.
-  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
-    Serial.println("WiFi connected");
-    wifi_connected = true;
+    // Check Wifi and init OTA.
+    if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+      Serial.println("WiFi connected");
+      wifi_connected = true;
 
-    // Port defaults to 3232
-    // ArduinoOTA.setPort(3232);
+      // Port defaults to 3232
+      // ArduinoOTA.setPort(3232);
 
-    // Hostname defaults to esp3232-[MAC]
-    // ArduinoOTA.setHostname("myesp32");
+      // Hostname defaults to esp3232-[MAC]
+      // ArduinoOTA.setHostname("myesp32");
 
-    // OTA setup
-    // Set password.
-    ArduinoOTA.setPassword(ota_pwd);
+      // OTA setup
+      // Set password.
+      ArduinoOTA.setPassword(ota_pwd);
 
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+      // Password can be set with it's md5 value as well
+      // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+      // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-    ArduinoOTA.onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH) {
-          type = "sketch";
-        } else {  // U_SPIFFS
-          type = "filesystem";
-        }
+      ArduinoOTA.onStart([]() {
+          String type;
+          if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+          } else {  // U_SPIFFS
+            type = "filesystem";
+          }
 
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        Serial.println("Start updating " + type);
-      });
+          // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+          Serial.println("Start updating " + type);
+        });
 
-      ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd");
-      });
+        ArduinoOTA.onEnd([]() {
+          Serial.println("\nEnd");
+        });
 
-      ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-      });
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+          Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
 
-      ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) {
-          Serial.println("Auth Failed");
-        } else if (error == OTA_BEGIN_ERROR) {
-          Serial.println("Begin Failed");
-        } else if (error == OTA_CONNECT_ERROR) {
-          Serial.println("Connect Failed");
-        } else if (error == OTA_RECEIVE_ERROR) {
-          Serial.println("Receive Failed");
-        } else if (error == OTA_END_ERROR) {
-          Serial.println("End Failed");
-        }
-      });
+        ArduinoOTA.onError([](ota_error_t error) {
+          Serial.printf("Error[%u]: ", error);
+          if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+          } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+          } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+          } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+          } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+          }
+        });
 
-    ArduinoOTA.begin();
+      ArduinoOTA.begin();
 
-    Serial.println("Ready");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+      Serial.println("Ready");
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
 
-  } else {
-    wifi_connected = false;
+    } else {
+      wifi_connected = false;
+    }
   }
-
   esp_led_blink(FREQ_READY, 4);
   
 }
@@ -159,30 +174,13 @@ void loop() {
 
   int volt_meas;
   int temp_meas;
+  int brightness_red;
+  int brightness_green;
+  int brightness_blue;
 
   // OTA loop
-  if (wifi_connected == true) {
-    ArduinoOTA.handle();
-  }
   
-  // Setup light mode 1, white, 100 brightness.
-  for (int n = 0; n<LED_BOTTOM_COUNT; n++) {
-    strip.setPixelColor(n, RED_DEFAULT, GREEN_DEFAULT, BLUE_DEFAULT);
-    if (digitalRead(HALL_PIN)) {
-      strip.setPixelColor(n, n*16, n*16, n*16);
-    }
-  };
-
-  if (wifi_connected == true) {
-    strip.setPixelColor(16, 40, 0, 0);
-    strip.setPixelColor(17, 40, 0, 0);
-  }
-  else {
-    for (int n = 16; n<18; n++) {
-      strip.setPixelColor(n, RED_DEFAULT, GREEN_DEFAULT, BLUE_DEFAULT);
-    };
-  };
-
+  // Measure temperature and battery voltage
   digitalWrite(ADC_MUX_PIN, ADC_MUX_TEMP);
   delay(1);
   temp_meas = analogRead(ADC_IN);
@@ -190,17 +188,67 @@ void loop() {
   delay(1);
   volt_meas = analogRead(ADC_IN);
 
-  if (temp_meas > 550) {
-    strip.setPixelColor(18, 0, 0, 100);
-  }
-  else if (temp_meas < 350) {
-    strip.setPixelColor(18, 100, 0, 0);
-  }
-  else {
-    strip.setPixelColor(18, 100-(temp_meas-350)/2, 0, ((temp_meas-350)/2));
+  if (temp_meas > 374) {
+    brightness_red   = RED_DEFAULT;
+    brightness_green = GREEN_DEFAULT;
+    brightness_blue  = BLUE_DEFAULT;
+  } else if (temp_meas < 337) {
+    brightness_red   = 10;
+    brightness_green = 10;
+    brightness_blue  = 10;
+  } else {
+    brightness_red   = 10 + ((temp_meas - 337) * 3);
+
   }
 
-  strip.setPixelColor(19, 255-volt_meas/4, volt_meas/4, 0);
+  // Setup light mode 1, white, 100 brightness.
+  for (int n = 0; n<LED_COUNT; n++) {
+    strip.setPixelColor(n, brightness_red, brightness_green, brightness_blue);
+    //if (digitalRead(HALL_PIN)) {
+    //  strip.setPixelColor(n, n*16, n*16, n*16);
+    //}
+  };
+
+  if (wifi_connected == true && wifi_configure == true) {
+    ArduinoOTA.handle();
+    strip.setPixelColor(16, 40, 0, 0);
+    strip.setPixelColor(17, 40, 0, 0);
+  }
+  else {
+    //for (int n = 16; n<18; n++) {
+    //  strip.setPixelColor(n, brightness_red, brightness_green, brightness_blue);
+    //};
+  };
+
+  if (digitalRead(HALL_PIN)) {
+    if (temp_meas > 550) {
+      strip.setPixelColor(18, 0, 0, 100);
+    }
+    else if (temp_meas < 350) {
+      strip.setPixelColor(18, 100, 0, 0);
+    }
+    else {
+      strip.setPixelColor(18, 100-(temp_meas-350)/2, 0, ((temp_meas-350)/2));
+    }
+
+    strip.setPixelColor(19, 255-volt_meas/4, volt_meas/4, 0);
+
+    int n_led_indicator;
+    if (volt_meas > 775) {
+      n_led_indicator = 15;
+    } else if (volt_meas < 553) {
+      n_led_indicator = 0;
+    } else {
+      n_led_indicator = (volt_meas - 553) / 14;
+    }
+    for (int n = 0; n<16; n++) {
+      if (n > n_led_indicator) {
+        strip.setPixelColor(n, 0, 0, 0);
+      } else {
+        strip.setPixelColor(n, brightness_red, brightness_green, brightness_blue);
+      }
+    }
+  }
 
   strip.show();
 
