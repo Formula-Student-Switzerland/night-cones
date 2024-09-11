8db4e30487fe208667d4597c8940a1c4ed07c018
ahorat <andreas.horat@alumni.ethz.ch>
2024-09-11 06:33:56 +0200

    CSW: Added credentials Template

---
+++ SW/client/_credentials.h
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define CRED_WIFI_SSID "SSID"
+#define CRED_WIFI_PW "PW"
+#define CRED_OTA_PW_MD5 "XXXX" // This is the MD5 Hash of the Password
+#define CRED_OTA_PW "XXXX"
+#endif
